c2181b44e1fa7a2ac82fbcc4527070fc8dbe513d
CoininDev <gabr.batz@gmail.com>
2025-02-19 22:09:35 -0300

    feat: enumerations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+enum Op{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+}
+
+enum Token{
+    Value(f64),
+    Operator(Op),
+    OpenParen,
+    CloseParen
+}
+
 fn main() {
     println!("Hello, world!");
 }
