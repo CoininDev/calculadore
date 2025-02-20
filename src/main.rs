f475822c8cb207743a32cc207cc860ec139936ec
CoininDev <gabr.batz@gmail.com>
2025-02-19 22:17:45 -0300

    feat: function tokenize

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ enum Op{
     Pow,
 }
 
+#[derive(Debug)]
 enum Token{
     Value(f64),
     Operator(Op),
@@ -14,6 +15,26 @@ enum Token{
     CloseParen
 }
 
+fn tokenize(expr: &str) -> Vec<Token> {
+    expr
+        .split_whitespace()
+        .map(|x| match x {
+            "+" => Token::Operator(Op::Add),
+            "-" => Token::Operator(Op::Sub),
+            "*" => Token::Operator(Op::Mul),
+            "/" => Token::Operator(Op::Div),
+            "^" => Token::Operator(Op::Pow),
+            "(" => Token::OpenParen,
+            ")" => Token::CloseParen,
+            _ => Token::Value(x
+                .parse::<f64>()
+                .expect("Invalid token")
+            ),
+        })
+        .collect()
+}
 fn main() {
-    println!("Hello, world!");
+    let expr = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
+    let tokens = tokenize(expr);
+    println!("{:?}", tokens);
 }
