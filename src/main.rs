4e890c24f0a4f1b858251f300b4f812f2536fb3f
CoininDev <gabr.batz@gmail.com>
2025-02-20 00:07:22 -0300

    feat: shunting yard function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ enum Op{
     Pow,
 }
 
+fn precedence(op: &Op) -> u8 {
+    match op {
+        Op::Add | Op::Sub => 1,
+        Op::Mul | Op::Div => 2,
+        Op::Pow => 3,
+    }
+}
+
 #[derive(Debug)]
 enum Token{
     Value(f64),
@@ -15,26 +23,64 @@ enum Token{
     CloseParen
 }
 
-fn tokenize(expr: &str) -> Vec<Token> {
+fn tokenize(expr: &str) -> Result<Vec<Token>, String> {
     expr
         .split_whitespace()
         .map(|x| match x {
-            "+" => Token::Operator(Op::Add),
-            "-" => Token::Operator(Op::Sub),
-            "*" => Token::Operator(Op::Mul),
-            "/" => Token::Operator(Op::Div),
-            "^" => Token::Operator(Op::Pow),
-            "(" => Token::OpenParen,
-            ")" => Token::CloseParen,
-            _ => Token::Value(x
-                .parse::<f64>()
-                .expect("Invalid token")
-            ),
+            "+" => Ok(Token::Operator(Op::Add)),
+            "-" => Ok(Token::Operator(Op::Sub)),
+            "*" => Ok(Token::Operator(Op::Mul)),
+            "/" => Ok(Token::Operator(Op::Div)),
+            "^" => Ok(Token::Operator(Op::Pow)),
+            "(" => Ok(Token::OpenParen),
+            ")" => Ok(Token::CloseParen),
+            num => num
+                            .parse::<f64>()
+                            .map(Token::Value)
+                            .map_err(|e| format!("Invalid Number: {}", e)),
         })
         .collect()
+}
+
+fn shunting_yard(tokens: Vec<Token>) -> Vec<Token> {
+    let (mut output, stack) = tokens
+        .into_iter()
+        .fold((Vec::new(), Vec::new()), |(mut output, mut stack), token| {
+            match token {
+                Token::Value(_) => {output.push(token);}
+                Token::Operator(op) => {
+                    // Desempilha operadores de maior ou igual precedência
+                    while let Some(Token::Operator(stack_op)) = stack.last() {
+                        if precedence(stack_op) >= precedence(&op) {
+                            output.push(stack.pop().unwrap());
+                        } else { break; }
+                    }
+                    stack.push(Token::Operator(op)); // Empilha o operador atual
+                }
+
+                Token::OpenParen => {stack.push(token);},
+                Token::CloseParen => {
+                    while let Some(token) = stack.pop() {
+                        if let Token::OpenParen = token { break; }
+                        output.push(token);
+                    }
+                }
+            }
+            (output, stack)
+        });
+
+    output.extend(stack.into_iter().rev());
+
+    output
 }
 fn main() {
     let expr = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
     let tokens = tokenize(expr);
-    println!("{:?}", tokens);
+    if let Ok(tokens) = &tokens {
+        println!("{:?}", tokens);
+    }
+
+    let rpn = shunting_yard(tokens.unwrap());
+    println!("{:?}", rpn);
+
 }
