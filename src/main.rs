4546c6146e3ec8a84074359028982dd8ced2c459
CoininDev <gabr.batz@gmail.com>
2025-02-28 14:46:38 -0300

    feat: Adicionando num_buffer e tornando a leitura independente de espaços

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::result;
-
 #[derive(Debug)]
-enum Op{
+enum Op {
     Add,
     Sub,
     Mul,
@@ -18,91 +16,136 @@ fn precedence(op: &Op) -> u8 {
 }
 
 #[derive(Debug)]
-enum Token{
+enum Token {
     Value(f64),
     Operator(Op),
     OpenParen,
-    CloseParen
+    CloseParen,
 }
 
 fn tokenize(expr: &str) -> Result<Vec<Token>, String> {
-    expr
-        .split_whitespace()
-        .map(|x| match x {
-            "+" => Ok(Token::Operator(Op::Add)),
-            "-" => Ok(Token::Operator(Op::Sub)),
-            "*" => Ok(Token::Operator(Op::Mul)),
-            "/" => Ok(Token::Operator(Op::Div)),
-            "^" => Ok(Token::Operator(Op::Pow)),
-            "(" => Ok(Token::OpenParen),
-            ")" => Ok(Token::CloseParen),
-            num => num
-                            .parse::<f64>()
-                            .map(Token::Value)
-                            .map_err(|e| format!("Invalid Number: {}", e)),
-        })
-        .collect()
+    let mut num_buffer:String = String::new();
+    let a = expr
+        .chars()
+        .filter(|x| { x.to_string() != " " })
+        ;
+
+    println!("{:?}", a);
+    let mut tokens = a.fold(vec![], |mut acc, token| {
+            match token {
+                '+' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::Operator(Op::Add));
+                },
+                '-' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::Operator(Op::Sub));
+                },
+                '*' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::Operator(Op::Mul));
+                },
+                '/' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::Operator(Op::Div));
+                },
+                '^' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::Operator(Op::Pow));
+                },
+                '(' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::OpenParen);
+                },
+                ')' => {
+                    check_reset_num_buffer(&mut num_buffer, &mut acc);
+                    acc.push(Token::CloseParen);
+                },
+                _ => {
+                    num_buffer.push(token);
+                }
+            };
+            acc
+        });
+
+    if num_buffer != "" {
+        check_reset_num_buffer(&mut num_buffer, &mut tokens);
+    }
+    Ok(tokens)
+}
+
+fn check_reset_num_buffer(num_buffer: &mut String, acc: &mut Vec<Token>) {
+    if num_buffer != "" {
+        acc.push(Token::Value(num_buffer.parse::<f64>().unwrap()));
+        *num_buffer = "".to_string();
+    }
 }
 
 fn shunting_yard(tokens: Vec<Token>) -> Vec<Token> {
-    let (mut output, stack) = tokens
-        .into_iter()
-        .fold((Vec::new(), Vec::new()), |(mut output, mut stack), token| {
+    let (mut output, stack) = tokens.into_iter().fold(
+        (Vec::new(), Vec::new()),
+        |(mut output, mut stack), token| {
             match token {
-                Token::Value(_) => {output.push(token);}
+                Token::Value(_) => {
+                    output.push(token);
+                }
                 Token::Operator(op) => {
                     // Desempilha operadores de maior ou igual precedência
                     while let Some(Token::Operator(stack_op)) = stack.last() {
                         if precedence(stack_op) >= precedence(&op) {
                             output.push(stack.pop().unwrap());
-                        } else { break; }
+                        } else {
+                            break;
+                        }
                     }
                     stack.push(Token::Operator(op)); // Empilha o operador atual
                 }
 
-                Token::OpenParen => {stack.push(token);},
+                Token::OpenParen => {
+                    stack.push(token);
+                }
                 Token::CloseParen => {
                     while let Some(token) = stack.pop() {
-                        if let Token::OpenParen = token { break; }
+                        if let Token::OpenParen = token {
+                            break;
+                        }
                         output.push(token);
                     }
                 }
             }
             (output, stack)
-        });
+        },
+    );
 
     output.extend(stack.into_iter().rev());
 
     output
 }
 
-fn evaluate(rpn: Vec<Token>) -> Result<f64, String>{
-    let mut result = rpn
-        .into_iter()
-        .fold(Ok(Vec::new()), |output, token| {
-            let mut output = output?;
+fn evaluate(rpn: Vec<Token>) -> Result<f64, String> {
+    let mut result = rpn.into_iter().fold(Ok(Vec::new()), |output, token| {
+        let mut output = output?;
 
-            match token {
-                Token::Value(val) => output.push(val),
-                Token::Operator(op) => {
-                    let r = output.pop().ok_or("Erro!")?;
-                    let l = output.pop().ok_or("err")?;
-                    let result = match op {
-                        Op::Add => l + r,
-                        Op::Sub => l - r,
-                        Op::Mul => l * r,
-                        Op::Div => l / r,
-                        Op::Pow => l.powf(r),
-                    };
-                    output.push(result);
-                }
-
-                _ => return Err("Invalid Token in RPN".to_string())
+        match token {
+            Token::Value(val) => output.push(val),
+            Token::Operator(op) => {
+                let r = output.pop().ok_or("Erro!")?;
+                let l = output.pop().ok_or("err")?;
+                let result = match op {
+                    Op::Add => l + r,
+                    Op::Sub => l - r,
+                    Op::Mul => l * r,
+                    Op::Div => l / r,
+                    Op::Pow => l.powf(r),
+                };
+                output.push(result);
             }
 
-            Ok(output)
-        })?
-    ;
+            _ => return Err("Invalid Token in RPN".to_string()),
+        }
+
+        Ok(output)
+    })?;
 
     if result.len() == 1 {
         Ok(result.pop().unwrap())
@@ -112,16 +155,10 @@ fn evaluate(rpn: Vec<Token>) -> Result<f64, String>{
 }
 
 fn main() {
-    let expr = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
-    let tokens = tokenize(expr);
-    if let Ok(tokens) = &tokens {
-        println!("Tokens: {:?}", tokens);
-    }
-
-    let rpn = shunting_yard(tokens.unwrap());
-    println!("RPN: {:?}", rpn);
-
+    let expr = "1+2 +(4-5)- (2/4)";
+    let tokens = tokenize(expr).unwrap();
+    println!("{:?}", tokens);
+    let rpn = shunting_yard(tokens);
     let result = evaluate(rpn);
-    println!("Resultado: {:?}", result);
-
+    println!("{:?}", result);
 }
